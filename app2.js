703359974aae53ff809b66db0e74182843d8aea8
NicoCamargo <nicolascamargod@gmail.com>
2023-01-17 22:10:38 -0500

    agregue dos líneas de código con el mismo texto agregando mi nombre al final

--- app2.js
+++ app2.js
@@ -1,3 +1,5 @@
 console.log("pueba");
 console.log("Generando el conflicto");
 console.log("Ahora a solucionarlo");
+console.log("Generando conflicto - Nico")
+console.log("Ahora a solucionarlo - Nico")
